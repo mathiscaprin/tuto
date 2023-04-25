dd937b5cbbf98367fb0cfb7ef7c15971a431b5c9
mathiscaprin <mathis.caprin@iadvize.com>
2023-04-25 14:12:06 +0200

    Add insertText and fix instance.setVariable call

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
+        <script src="https://static.iadvize.com/conversation-panel-app-lib/2.9.0/idzcpa.umd.production.min.js"></script>
         <Main />
         <NextScript />
       </body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,27 @@
 import { useEffect } from "react"
 import Singleton from "./designpattern/singleton"
+import { AnyAaaaRecord } from "dns"
 
 const instance = Singleton.getInstance()
 
 export default function Home() {
 
   useEffect(()=>{
-    instance.setVariable = (window as any).idzCpa.init()
+    instance.setVariable((window as any).idzCpa.init())
+    console.log(instance.getVariable())
+
   })
+
+  function insertText(){
+    console.log(instance.getVariable())
+    instance.getVariable().then((client : any)=>{
+      client.insertTextInComposeBox("Hello World")
+    })
+  }
 
   return (
     <div>
-      <button>Envoyer</button>
+      <button onClick={insertText}>Envoyer</button>
     </div>
   )
 }
