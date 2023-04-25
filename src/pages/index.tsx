fafa2f9bc252df9b30970f6e1d2703f04fd8e4c2
mathiscaprin <mathis.caprin@iadvize.com>
2023-04-25 15:08:44 +0200

    add product, add card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,72 @@
 import { useEffect } from "react"
 import Singleton from "./designpattern/singleton"
-import { AnyAaaaRecord } from "dns"
-
+import { coffees, Product } from "./products";
 const instance = Singleton.getInstance()
+
+type Action = {
+  type: "LINK";
+  title: string;
+  url: string;
+};
+
+type Card = {
+  title?: string;
+  text?: string;
+  actions: Action[];
+  image?: {
+      url: string;
+      description: string;
+  }
+};
 
 export default function Home() {
 
   useEffect(()=>{
     instance.setVariable((window as any).idzCpa.init())
-    console.log(instance.getVariable())
-
   })
 
   function insertText(){
-    console.log(instance.getVariable())
     instance.getVariable().then((client : any)=>{
       client.insertTextInComposeBox("Hello World")
     })
   }
 
+  function insertCard(coffee : Product){
+    const card : Card = {
+      title : coffee.name,
+      text : coffee.description,
+      
+      actions : [
+        {
+          type : "LINK",
+          title : coffee.name,
+          url : coffee.link
+        }
+      ],
+      image : {
+        url : coffee.picture,
+        description : coffee.name 
+      }
+
+    }
+    instance.getVariable().then((client : any)=>{
+      client.pushCardInConversationThread(card)
+    })
+  }
+
+  const list = coffees.map((coffee)=>{
+    return(
+      <div>
+        <button onClick={() => insertCard(coffee)}>Envoyer {coffee.name}</button>
+      </div>
+    )
+  })
+
+
   return (
     <div>
-      <button onClick={insertText}>Envoyer</button>
+      <button onClick={() => insertText}>Envoyer</button>
+      {list}
     </div>
   )
 }
