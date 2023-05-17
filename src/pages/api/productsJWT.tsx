1712cf54e03dce00cf34fcb29c5d47c970292cc9
mathiscaprin <mathis.caprin@iadvize.com>
2023-05-17 11:27:44 +0200

    add getStarted

--- src/pages/api/productsJWT.tsx
+++ src/pages/api/productsJWT.tsx
@@ -1,27 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import Cors from 'cors'
 import { jwtMiddleware } from './jwtMiddleware'
-
-
-const cors = Cors({
-    methods: ['POST', 'GET', 'HEAD'],
-})
-
-function runMiddleware(
-    req: NextApiRequest,
-    res: NextApiResponse,
-    fn: Function
-  ) {
-    return new Promise((resolve, reject) => {
-      fn(req, res, (result: any) => {
-        if (result instanceof Error) {
-          return reject(result)
-        }
-        return resolve(result)
-      })
-    })
-  }
-
 
 export type Product = {
     id : number,
@@ -100,7 +78,6 @@ export const coffees : Product[] = [
 ]
 
 const handler = async (req : NextApiRequest,res : NextApiResponse) => {
-  runMiddleware(req,res,cors)
   res.status(200).json(coffees.map((coffee) => ({
     id: coffee.id,
     name: coffee.name,
